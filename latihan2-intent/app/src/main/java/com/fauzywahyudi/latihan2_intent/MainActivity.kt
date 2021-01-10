16133a0fcfde3785f8b27e7751e8400ce5955652
Fauzy Wahyudi <fauzy.wahyudi79@gmail.com>
2021-01-10 07:56:52 +0700

    add latihan1, latihan 2

---
+++ latihan2-intent/app/src/main/java/com/fauzywahyudi/latihan2_intent/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.fauzywahyudi.latihan2_intent
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnMoveActivity : Button = findViewById(R.id.btn_move_activity)
+        val btnMoveWithDataActivity : Button = findViewById(R.id.btn_move_with_data)
+        val btnDialPhone: Button = findViewById(R.id.btn_dial_number)
+        btnMoveActivity.setOnClickListener(this)
+        btnMoveWithDataActivity.setOnClickListener(this)
+        btnDialPhone.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id){
+            R.id.btn_move_activity -> {
+                val moveIntent = Intent(this@MainActivity, MoveActivity::class.java)
+                startActivity(moveIntent)
+            }
+            R.id.btn_move_with_data -> {
+                val moveWithDataIntent = Intent(this@MainActivity,MoveWithDataActivity::class.java)
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_NAME, "Fauzy Wahyudi")
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_AGE, 23)
+                startActivity(moveWithDataIntent)
+            }
+            R.id.btn_dial_number -> {
+                val phoneNumber = "085264033783"
+                val dialPhoneIntent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:$phoneNumber"))
+                startActivity(dialPhoneIntent)
+            }
+        }
+    }
+}
